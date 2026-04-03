49b7f136bebaad3f0839531c074d7a675d6362de
Pujan-Pandey <pujanpandey21@gmail.com>
2026-04-03 16:36:12 +0545

    feat(auth_middleware): refactor auth middleware

---
+++ Server/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.cookies?.token;
+        console.log(token)
+        if (!token) {
+            return res.status(404).json({
+                success: false,
+                message: "Token not found!"
+            })
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(404).json({
+            success: false,
+            message: "Unauthenticated !"
+        })
+
+    }
+}
+module.exports = authMiddleware;
