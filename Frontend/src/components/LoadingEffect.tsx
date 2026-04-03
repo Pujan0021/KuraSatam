9638075564ba1bb010c7f6247eaee8fffee8b2b5
Pujan-Pandey <pujanpandey21@gmail.com>
2026-04-03 13:03:15 +0545

    feat(loading-effect):develop ui design of loading-effect

---
+++ Frontend/src/components/LoadingEffect.tsx
@@ -0,0 +1,11 @@
+import { Slab } from "react-loading-indicators";
+
+const LoadingEffect = () => {
+  return (
+    <>
+      <Slab color="#342d2d" size="small" text="" textColor="" />
+    </>
+  );
+};
+
+export default LoadingEffect;
