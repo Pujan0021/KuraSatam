ef34327039ec9ada71e5523cfe0447dfe389b1e1
Pujan-Pandey <pujanpandey21@gmail.com>
2026-04-05 17:23:07 +0545

    json res fixed

--- Server/src/routes/message.route.js
+++ Server/src/routes/message.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Message = require("../models/Message.model");
+const Users = require("../models/Users.model");
 const jwt = require("jsonwebtoken");
 
 router.post("/sendMessage/:id", async (req, res) => {
-    const token = req.cookies.token;
     const receiver = req.params.id;
 
     try {
@@ -28,10 +28,21 @@ router.post("/sendMessage/:id", async (req, res) => {
 router.get("/message/:id", async (req, res) => {
     try {
         const receiver = req.params.id;
+        console.log(receiver);
         const messages = await Message.find({ receiver });
         res.json(messages);
     } catch (err) {
         res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
 });
+
+router.get("/users", async (req, res) => {
+    try {
+        const users = await Users.find();
+        console.log(users);
+        res.status(200).json(users)
+    } catch (err) {
+        console.log(err, "Error occured!")
+    }
+})
 module.exports = router;
